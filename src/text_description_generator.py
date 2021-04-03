aa227b0826b75726c5462d0cfe08d261e3261f55
Karen Chung <seoyeon@mit.edu>
2021-04-03 14:33:01 -0400

    some preliminary descriptors

--- src/text_description_generator.py
+++ src/text_description_generator.py
@@ -5,26 +5,46 @@ import pandas as pd
 filename = r'data\plants_usda_selected_features.txt'
 read_file = pd.read_csv(filename, error_bad_lines=False)
 
-names = pd.Series.tolist(read_file["Accepted Symbol"])
-areas = pd.Series.tolist(read_file["PLANTS Floristic Area"])
-categories = pd.Series.tolist(read_file["Category"])
-kingdoms = pd.Series.tolist(read_file["Kingdom"])
-durations = pd.Series.tolist(read_file["Duration"])
-habits = pd.Series.tolist(read_file["Growth Habit"])
-growth_periods = pd.Series.tolist(read_file["Active Growth Period"])
-regrowth_rates = pd.Series.tolist(read_file["After Harvest Regrowth Rate"])
-fall_conspicuouses = pd.Series.tolist(read_file["Fall Conspicuous"])
+def get_attributes(feature):
+    return pd.Series.tolist(read_file[feature])
+
+def isify(s, o=["yes","no"],r=["is, isn't"]):
+    for i in range(len(o)):
+        if(o[i]==s):
+            return r[i]
+        return ""
 
 
-for i in range(1, 10): #len(names)
+names = get_attributes("Accepted Symbol")
+areas = get_attributes("PLANTS Floristic Area")
+categories = get_attributes("Category")
+kingdoms = get_attributes("Kingdom")
+durations = get_attributes("Duration")
+habits = get_attributes("Growth Habit")
+growth_periods = get_attributes("Active Growth Period")
+regrowth_rates = get_attributes("After Harvest Regrowth Rate")
+fall_conspicuouses = get_attributes("Fall Conspicuous")
+fire_resistances = get_attributes("Fire Resistance")
+flower_colors = get_attributes("Flower Color")
+flower_conspicuouses = get_attributes("Flower Conspicuous")
+foliage_colors = get_attributes("Foliage Color")
+foliage_porosities_summer = get_attributes("Foliage Porosity Summer")
+foliage_porosities_winter = get_attributes("Foliage Porosity Winter")
+foliage_texture = get_attributes("Foliage Porosity Summer")
+
+
+
+for i in range(3, 10): #len(names)
     name = names[i]
     filename = f'data\{name}_{0}.txt'
     descriptions = open(filename, 'w+')  # change w+ mode later (overriding every time for now)
 
-
+    import numpy as np
     prefix = "A plant "
+    if str(areas[i]) == 'nan':
+        print()
     area = f"that lives in {areas[i]}."
-    s1 = prefix + area + '\n'
+    s1 = (prefix + area + '\n') if str(areas[i]) != 'nan' else ""
 
     category = f"A plant categorized as {categories[i]} "
     kingdom = f"and is a {kingdoms[i]}." # fungus, plantae
@@ -32,12 +52,14 @@ for i in range(1, 10): #len(names)
 
     duration = f"A {durations[i]} plant that grows as "
     habit = f"{habits[i]} during the "
-    growth_period = f"{growth_periods[i]}, and is "
-    if fall_conspicuouses[i] == 'Yes':
-        fall_conspicuous = f"conspicuous during the fall."
-    else:
-        fall_conspicuous = f"not conspicuous during the fall."
+    growth_period = f"{growth_periods[i]}, and "
+    fall_conspicuous = f"{isify(fall_conspicuouses[i])} conspicuous during the fall \n"
     s3 = duration + habit + growth_period + fall_conspicuous + '\n'
+
+
+
+
+
 
     print(s1)
     print(s2)
