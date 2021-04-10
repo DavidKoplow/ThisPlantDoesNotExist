299c20a276193576c8adea2e943540e43a7b70fb
seoyeon <seoyeon@mit.edu>
2021-04-10 13:36:55 -0400

    all descriptions prelim

--- src/text_description_generator.py
+++ src/text_description_generator.py
@@ -13,8 +13,14 @@ def isify(s, o=["yes","no"],r=["is, isn't"]):
         return ""
     for i in range(len(o)):
         if(o[i]==s):
-            return before+" "+r[i]+" "+after
+            return " "+r[i]+" "
     return ""
+
+def isify_(s, o=["yes","no"],r=["is, isn't"]):
+   for i in range(len(o)):
+       if(o[i]==s):
+           return r[i]
+       return "is unknown if"
 
 
 def isnan(input):
@@ -38,7 +44,7 @@ flower_conspicuouses = get_attributes("Flower Conspicuous")
 foliage_colors = get_attributes("Foliage Color")
 foliage_porosities_summer = get_attributes("Foliage Porosity Summer")
 foliage_porosities_winter = get_attributes("Foliage Porosity Winter")
-foliage_texture = get_attributes("Foliage Porosity Summer")
+foliage_textures = get_attributes("Foliage Porosity Summer")
 fruit_colors = get_attributes("Fruit Color")
 fruit_conspicuouses = get_attributes("Fruit Conspicuous")
 growth_forms = get_attributes("Growth Form")
@@ -50,23 +56,24 @@ lifespans = get_attributes("Lifespan")
 low_growing_grasses = get_attributes("Low Growing Grass")
 shape_and_orientations = get_attributes("Shape and Orientation")
 toxicities = get_attributes("Toxicity")
-drought  =  pd.Series.tolist(read_file["Drought Tolerance"]) 
+drought  =  pd.Series.tolist(read_file["Drought Tolerance"])
 fertility =  pd.Series.tolist(read_file["Fertility Requirement"]) 
 fire_tolerance  =  pd.Series.tolist(read_file["Fire Tolerance"])
 precipitation1  =  pd.Series.tolist(read_file["Precipitation (Minimum)"])
-precipitation1  =  pd.Series.tolist(read_file["Precipitation (Maximum)"])
+precipitation2  =  pd.Series.tolist(read_file["Precipitation (Maximum)"])
 
-shade =  pd.Series.tolist(read_file["Shade Tolerance"])
+shades =  pd.Series.tolist(read_file["Shade Tolerance"])
 tempature = pd.Series.tolist(read_file["Temperature, Minimum (°F)"])
-period = pd.Series.tolist(read_file["Bloom Period"])
-bulb = pd.Series.tolist(read_file["Propogated by Bulbs"])
+periods = pd.Series.tolist(read_file["Bloom Period"])
+bulbs = pd.Series.tolist(read_file["Propogated by Bulbs"])
 
 #product =  pd.Series.tolist(read_file["Berry/Nut/Seed Product"])
-palatable = pd.Series.tolist(read_file["Palatable Human"])
+palatables = pd.Series.tolist(read_file["Palatable Human"])
 
 
 
-for i in range(3, 10): #len(names)
+for i in range(3, 10):
+# change to for i in range(len(names)):
     name = names[i]
     filename = f'data\{name}_{0}.txt'
     descriptions = open(filename, 'w+')  # change w+ mode later (overriding every time for now)
@@ -81,12 +88,12 @@ for i in range(3, 10): #len(names)
     duration = f"{durations[i]}ly " if not isnan(str(durations[i])) else ""
     habit = f"as {habits[i]} " if not isnan(str(habits[i])) else ""
     growth_period = f"in the {growth_periods[i]} " if not isnan(str(growth_periods[i])) else ""
-    fall_conspicuous = f",{isify(fall_conspicuouses[i])} conspicuous during the fall"
+    fall_conspicuous = f",{isify_(fall_conspicuouses[i])} conspicuous during the fall"
     s_growth = prefix + "that grows " + duration + habit + growth_period + fall_conspicuous + '.\n'
 
-    fire_resistance = f"{isify(fire_resistances[i])} resistant to fire " if not isnan(str(fire_resistances[i])) else ""
+    fire_resistance = f"{isify_(fire_resistances[i])} resistant to fire " if not isnan(str(fire_resistances[i])) else ""
     flower_color = f"with {flower_colors[i]} flower color " if not isnan(str(flower_colors[i])) else ""
-    flower_conspicuous = f"with flower that {isify(flower_conspicuouses[i])} resistant to fire " if not isnan(str(flower_conspicuouses[i])) else ""
+    flower_conspicuous = f"with flower that {isify_(flower_conspicuouses[i])} resistant to fire " if not isnan(str(flower_conspicuouses[i])) else ""
     s_flower = prefix + fire_resistance + flower_color + flower_conspicuous + '.\n'
 
     foliage_color = f"that is {foliage_colors[i]}, " if not isnan(str(foliage_colors[i])) else ""
@@ -95,19 +102,19 @@ for i in range(3, 10): #len(names)
     foliage_porosity_winter = f"and {foliage_porosities_winter[i]} over the winter" if not isnan(str(foliage_porosities_summer[i])) else ""
     s_foliage = prefix + "with foliage " + foliage_color + foliage_texture + foliage_porosity_summer + foliage_porosity_winter + '.\n'
 
-    fruit_color = f"A plant that has {fruit_color[i]} colored fruit and "
-    fruit_conspicuous = f"its' fruit {isify(fruit_conspicuouses[i])} conspicuous \n"
+    fruit_color = f"A plant that has {fruit_colors[i]} colored fruit and "
+    fruit_conspicuous = f"its' fruit {isify_(fruit_conspicuouses[i])} conspicuous \n"
     fruit = fruit_color + fruit_conspicuous + '\n'
 
     growth_form = f"A plant that grows in {growth_forms[i]} form and "
-    growth_rate = f"at a {growth_rate[i]} rate \n"
+    growth_rate = f"at a {growth_rates[i]} rate \n"
     growth = growth_form + growth_rate + '\n'
 
     start_height = f"A plant that starts at {start_heights[i]} feet tall "
-    mature_height = f"and its' mature height is {mature_height[i]} feet tall \n"
+    mature_height = f"and its' mature height is {mature_heights[i]} feet tall \n"
     height = start_height + mature_height + '\n'
 
-    if isify(leaf_retention[i]) == "":
+    if isify(leaf_retentions[i]) == "":
         leaf_retention = f"A plant that does not retain leaves "
     else:
         leaf_retention = f"A plant that retains leaves "
@@ -120,18 +127,18 @@ for i in range(3, 10): #len(names)
     shape_and_orientation = f"with a {shape_and_orientations[i]} orientation \n"
     leaves_life_shape = leaf_retention + lifespan + toxicity + low_growing_grass + shape_and_orientation
 
-    drought="that is "+ isify(drought[i],s=["low","medium","high"],r=["not","somewhat","very"] +" tolorant of drought")
-    fertility = "that "+isify(fire_tolerance[i],s=["low","medium","high"],r=["doesn't require fertle","requires normal","requires fertle"])+" soil to grow"
-    fire_tolerance = "that is "+isify(fire_tolerance[i],s=["med","low","high"],r=["somewhat","isn't","is"])+" tolerant to fire"
-    precipitation = "needs between "+precipitation1[i]+" and "+precipitation2[i]+" precipitation "
-    shade = "that is "+shade[i]+"of shade"
-    tempature = "that survives at "+"Temperature, Minimum (°F)"
-    period = "that blooms in "+period[i]
-    bulb = "that "+isify(bulb[i],r=["has",""])+ "bulbs"
-    palatable = "that "+isify(palatable[i])+" edible"
+    drought="that is "+ isify(drought[i],o=["low","medium","high"],r=["not","somewhat","very"]) +" tolorant of drought"
+    fertility = "that "+isify(fire_tolerance[i],o=["low","medium","high"],r=["doesn't require fertle","requires normal","requires fertle"])+" soil to grow"
+    fire_tolerance = "that is "+isify(fire_tolerance[i],o=["med","low","high"],r=["somewhat","isn't","is"])+" tolerant to fire"
+    precipitation = "needs between "+ str(precipitation1[i]) +" and "+str(precipitation2[i])+" precipitation "
+    shade = "that is "+str(shades[i])+"of shade"
+    temperature = "that survives at "+"Temperature, Minimum (°F)"
+    # period = "that blooms in "+periods[i]
+    bulb = "that "+isify(bulbs[i],r=["has",""])+ "bulbs"
+    palatable = "that "+isify(palatables[i])+" edible"
 
 
-    L = [s1, s2, s3, fruit, growth, height, leaves_life_shape]
+    L = [s_category, s_growth, s_flower, fruit, growth, height, leaves_life_shape, drought, fertility, fire_tolerance, precipitation, shade, temperature, bulb, palatable]
     print(L)
     descriptions.writelines(L)
     descriptions.close
