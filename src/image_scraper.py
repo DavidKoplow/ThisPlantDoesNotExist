8bbac07685eb1597287d0e1103a0b56c94717319
Dylan Walker <dwscout1@gmail.com>
2021-05-01 18:04:11 -0400

    Finished New Image Scraper

--- src/image_scraper.py
+++ src/image_scraper.py
@@ -1,7 +1,8 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -28,64 +29,34 @@ DRIVER_PATH = 'Image Scraper/geckodriver.exe'
 
 
 # Gets each image url from the website by first navigating to the images tab and then clicking on each image. Also sorts out non-plant (specifically non gallery urls) images.
-def fetch_image_urls(query, max_links_to_fetch, wd, sleep_between_interactions):
-    def scroll_to_end(wd):
-        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)
-
+def fetch_image_urls(query, max_links_to_fetch, wd):
     search_url = 'https://plantsservices.sc.egov.usda.gov/api/PlantProfile?symbol={q}'
-    # load the page
-    wd.get(search_url.format(q=query))
+    search = search_url.format(q=query)
+    #print (search)
+    wd.execute_script('''window.open("https://google.com", "_blank");''')
+    wd.switch_to.window(wd.window_handles[1])
+    wd.get(search)
+    #print (wd.window_handles)
     wd.maximize_window()
-    lines = wd.find_element_by_tag_name("AcceptedId")
-    print (lines)
+    lines = WebDriverWait(wd, 10).until(EC.presence_of_element_located((By.TAG_NAME, "AcceptedId")))
     tag_url = "https://plantsservices.sc.egov.usda.gov/api/PlantImages?plantId={q}"
-    #wd.get(tag_url.format(q=ID))
+    wd.get(tag_url.format(q=lines.text))
     image_urls = set()
     image_count = 0
     results_start = 0
-    while image_count < max_links_to_fetch:
-        
-        image_tab = wait(wd, 10).until(EC.visibility_of_element_located((By.ID, "ImagesTab")))
-        #tabs = wd.find_element_by_class_name('nav nav-pills justify-content-start')
-        #print(tabs)
-        #image_tab = tabs.find_elements_by_class_name('nav-item')
-        image_tab.click()
-        # get all image thumbnail results
-        scroll_to_end(wd)
-        thumbnail_results = wd.find_elements_by_partial_link_text("https://plants.sc.egov.usda.gov/ImageLibrary")
-        #thumbnail_results = wd.find_elements_by_class_name('ngx-gallery-thumbnail ngx-gallery-clickable')
-        #thumbnail_results.append(wd.find_element_by_class_name('ngx-gallery-thumbnail ngx-gallery-active ngx-gallery-clickable'))
-        number_results = len(thumbnail_results)
-        print (thumbnail_results)
-
-        #print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
-
-        for img in thumbnail_results[results_start:number_results]:
-            # try to click every thumbnail such that we can get the real image behind it
-            try:
-                img.click()
-                time.sleep(sleep_between_interactions)
-            except Exception:
-                continue
-
-            # extract image urls
-            actual_images = wd.find_elements_by_css_selector('img')
-            for actual_image in actual_images:
-                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                    if "/gallery/" in actual_image.get_attribute('src'):
-                        image_urls.add(actual_image.get_attribute('src'))
-
-            image_count = len(image_urls)
-
-            if len(image_urls) >= max_links_to_fetch:
-                #print(f"Found: {len(image_urls)} image links, done!")
-                break
-        # move the result startpoint further down
-        results_start = len(thumbnail_results)
-
-    #wd.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w') 
-
+    thumbnail_results = wd.find_elements_by_tag_name("StandardSizeImageLibraryPath")
+    number_results = len(thumbnail_results)
+    print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+    for img in thumbnail_results[results_start:number_results]:
+        image_url = "https://plants.sc.egov.usda.gov{q}"
+        image_urls.add(image_url.format(q=img.text))
+        image_count = len(image_urls)
+    print(f"Found: {len(image_urls)} image links, done!")
+            
+    results_start = len(thumbnail_results)
+    #close tab
+    wd.close()  
+    wd.switch_to.window(wd.window_handles[0])
     return image_urls
 
 
@@ -110,12 +81,10 @@ def persist_image(folder_path, url, plant_name, iter):
                               ImageChops.difference(rgb[0], rgb[2]).getextrema()[1]
         if (largest_diff_pixels > 50):
             im3=rgb[2].resize((1,1))
-            #print(im3)
             if(im3.getpixel((0,0))<140):
                 file_path = os.path.join(folder_path, plant_name + '_' + str(iter) + '.jpg')
                 with open(file_path, 'wb') as f:
                     image.save(f, "JPEG", quality=85)
-                #print(f"SUCCESS - saved {url} - as {file_path}")
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
@@ -127,13 +96,7 @@ def search_and_download(search_term, driver_path=DRIVER_PATH, target_path=path, 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
-    global wd
-    wd= webdriver.Firefox(executable_path=driver_path)
-    res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
-    #with webdriver.Chrome(executable_path=driver_path, options=chrome_options) as wd:
-        #global wd
-        #res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0)
-
+    res = fetch_image_urls(search_term, number_images, wd=wd)
     i = 0
     for elem in res:
         persist_image(target_folder, elem, search_term, i)
@@ -142,5 +105,10 @@ def search_and_download(search_term, driver_path=DRIVER_PATH, target_path=path, 
 
 # Change the argument here to whatever plant ID that you want to search
 plant_names = plant_info_generator(r'data\plants_usda_selected_features.txt')
+
+wd= webdriver.Firefox(executable_path=DRIVER_PATH)
+wd.get('http://thisplantdoesntexist.surge.sh/?fbclid=IwAR0xw5B239O_v7GpT0w_qCeBDCJUCXvIWU3X6NIVy0va77sjWqpCb0cJtms')
+
 for name in plant_names:
     search_and_download(name)
+    print(name)
