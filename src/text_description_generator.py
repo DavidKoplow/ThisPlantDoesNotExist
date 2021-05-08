5a86575f755ad03ff14cc14df57c05f62d770cd9
Aegis100 <dwscout1@gmail.com>
2021-05-08 14:11:17 -0400

    Update text_description_generator.py

--- src/text_description_generator.py
+++ src/text_description_generator.py
@@ -35,6 +35,7 @@ def isnan(input):
 
 
 names = get_attributes("Accepted Symbol")
+scientific_names = get_attributes("Scientific Name")
 areas = get_attributes("PLANTS Floristic Area")
 categories = get_attributes("Category")
 kingdoms = get_attributes("Kingdom")
@@ -79,60 +80,62 @@ root="C:/Users/Administrator/Documents/GitHub/data"
 
 for i in range(3, len(names)):
 #for i in range(3, 10):
-    prefix = "A plant "
-
-    area = f"that lives in {areas[i]} " if not isnan(str(areas[i])) else "" # change this to unabbreviated place names
+    #prefix = "A plant "
+    scientific_name = f"{scientific_names[i]} is "
+    #area = f"that lives in {areas[i]} " if not isnan(str(areas[i])) else "" # change this to unabbreviated place names
     category = f"categorized as {categories[i]} " if not isnan(str(categories[i])) else ""
-    kingdom = f"in the {kingdoms[i]} kingdom" if not isnan(str(kingdoms[i])) else "" # fungus, plantae
-    s_category = prefix + area + category + kingdom + '.\n'
+    kingdom = f"in the {kingdoms[i]} kingdom " if not isnan(str(kingdoms[i])) else "" # fungus, plantae
+    flower_color = f"with {isknown(flower_colors[i])} flower color. " if not isnan(str(flower_colors[i])) else ""
+    s_category = area + category + kingdom + flower_color + '.\n'
 
-    duration = prefix + f"that grows {durations[i]}ly \n" if not isnan(str(durations[i])) else ""
-    habit = prefix + f"that grows as {habits[i]} \n" if not isnan(str(habits[i])) else ""
-    growth_period = prefix + f"that grows in the {growth_periods[i]} \n" if not isnan(str(growth_periods[i])) else "\n"
-    fall_conspicuous = prefix + f"that is {isify_(fall_conspicuouses[i])} conspicuous during the fall \n"
+    #duration = prefix + f"that grows {durations[i]}ly \n" if not isnan(str(durations[i])) else ""
+    #habit = prefix + f"that grows as {habits[i]} \n" if not isnan(str(habits[i])) else ""
+    #growth_period = prefix + f"that grows in the {growth_periods[i]} \n" if not isnan(str(growth_periods[i])) else "\n"
+    #fall_conspicuous = prefix + f"that is {isify_(fall_conspicuouses[i])} conspicuous during the fall \n"
 
-    flower_color = prefix + f"with {isknown(flower_colors[i])} flower color. \n"
-    flower_conspicuous = prefix + f"with flower that {isify_(flower_conspicuouses[i])} conspicuous.\n"
-    foliage_color = prefix + f"with {isknown(foliage_colors[i])} foliage color. \n"
-    foliage_texture = prefix + f"with {isknown(foliage_textures[i])} foliage texture. \n"
-    foliage_porosity = prefix + f"with {isknown(foliage_porosities_summer[i])} foliage porosity over the summer and {isknown(foliage_porosities_summer[i])} foliage porosity over the winter. \n"
+    
+    #flower_conspicuous = prefix + f"with flower that {isify_(flower_conspicuouses[i])} conspicuous.\n"
+    #foliage_color = prefix + f"with {isknown(foliage_colors[i])} foliage color. \n"
+    #foliage_texture = prefix + f"with {isknown(foliage_textures[i])} foliage texture. \n"
+    #foliage_porosity = prefix + f"with {isknown(foliage_porosities_summer[i])} foliage porosity over the summer and {isknown(foliage_porosities_summer[i])} foliage porosity over the winter. \n"
     #s_foliage = prefix + "with foliage " + foliage_color + foliage_texture + foliage_porosity_summer + foliage_porosity_winter + '.\n'
 
-    fruit_color = f"A plant that has {isknown(fruit_colors[i])} colored fruit. \n"
-    fruit_conspicuous = f"A plant with fruit that {isify_(fruit_conspicuouses[i])} conspicuous. \n"
+    #fruit_color = f"A plant that has {isknown(fruit_colors[i])} colored fruit. \n"
+    #fruit_conspicuous = f"A plant with fruit that {isify_(fruit_conspicuouses[i])} conspicuous. \n"
 
-    growth_form = prefix + f"that grows in {isknown(growth_forms[i])} form." + '\n'
-    growth_rate = prefix + f"that grows at a {isknown(growth_rates[i])} rate. \n"
+    #growth_form = prefix + f"that grows in {isknown(growth_forms[i])} form." + '\n'
+    #growth_rate = prefix + f"that grows at a {isknown(growth_rates[i])} rate. \n"
 
-    start_height = prefix + f"that starts at {isknown(start_heights[i])} feet tall. \n"
-    mature_height = f"that has mature height {mature_heights[i]} feet tall. \n"
-    height = start_height + "and" + mature_height
+    #start_height = prefix + f"that starts at {isknown(start_heights[i])} feet tall. \n"
+    #mature_height = f"that has mature height {mature_heights[i]} feet tall. \n"
+    #height = start_height + "and" + mature_height
 
-    leaf_retention = prefix + "that " + isify_(leaf_retentions[i], o=["Yes", "No"],
-                                    r=["does", "does not"]) + " retaining leaves" + '.\n'
-    lifespan = prefix + f"that has a {isknown(lifespans[i])} lifespan.\n"
-    toxicity = prefix + f"that {isify_(toxicities[i])} toxic.\n"
-    low_growing_grass = prefix + f"that {isify_(low_growing_grasses[i])} low growing grass. \n"
-    shape_and_orientation = prefix + f"that has a {isknown(shape_and_orientations[i])} orientation." + "\n"
+    #leaf_retention = prefix + "that " + isify_(leaf_retentions[i], o=["Yes", "No"],
+                                    #r=["does", "does not"]) + " retaining leaves" + '.\n'
+    #lifespan = prefix + f"that has a {isknown(lifespans[i])} lifespan.\n"
+    #toxicity = prefix + f"that {isify_(toxicities[i])} toxic.\n"
+    #low_growing_grass = prefix + f"that {isify_(low_growing_grasses[i])} low growing grass. \n"
+    #shape_and_orientation = prefix + f"that has a {isknown(shape_and_orientations[i])} orientation." + "\n"
     #leaves_life_shape = prefix + leaf_retention + lifespan + toxicity + low_growing_grass + shape_and_orientation + '.\n'
 
-    drought = prefix + "that " + isify_(droughts[i],o=["None", "Low","Medium","High"],r=["not","slightly", "moderately","very"]) +" tolerant of drought" + '.\n'
-    fertility = prefix + "that "+ isify_(fertilities[i])+" requiring fertile soil to grow" + '.\n'
-    fire_tolerance = prefix + "that "+ isify_(fire_tolerances[i],o=["None", "Low","Medium","High"],r=["not","slightly", "moderately","very"])+" tolerant of fire" + '.\n'
-    precipitation = prefix + "that needs between "+ str(precipitation1s[i]) +" and "+str(precipitation2s[i])+" precipitation " + '.\n'
-    shade = prefix + "that "+ isify_(str(shades[i])) +" providing of shade" + '.\n'
-    temperature = prefix + "that survives at "+ isknown(temperatures[i]) + " degrees, minimum (°F)" + '.\n'
-    bulb = prefix + "that" + isify_(bulbs[i],r=["has","does not have"])+ "bulbs" + '.\n'
-    palatable = prefix + "that "+isify_(palatables[i])+ " edible" + '.\n'
+    #drought = prefix + "that " + isify_(droughts[i],o=["None", "Low","Medium","High"],r=["not","slightly", "moderately","very"]) +" tolerant of drought" + '.\n'
+    #fertility = prefix + "that "+ isify_(fertilities[i])+" requiring fertile soil to grow" + '.\n'
+    #fire_tolerance = prefix + "that "+ isify_(fire_tolerances[i],o=["None", "Low","Medium","High"],r=["not","slightly", "moderately","very"])+" tolerant of fire" + '.\n'
+    #precipitation = prefix + "that needs between "+ str(precipitation1s[i]) +" and "+str(precipitation2s[i])+" precipitation " + '.\n'
+    #shade = prefix + "that "+ isify_(str(shades[i])) +" providing of shade" + '.\n'
+    #temperature = prefix + "that survives at "+ isknown(temperatures[i]) + " degrees, minimum (°F)" + '.\n'
+    #bulb = prefix + "that" + isify_(bulbs[i],r=["has","does not have"])+ "bulbs" + '.\n'
+    #palatable = prefix + "that "+isify_(palatables[i])+ " edible" + '.\n'
 
 
-    L = [s_category] + [duration, habit, growth_period, fall_conspicuous] + [flower_color, flower_conspicuous] + \
-        [foliage_color, foliage_texture, foliage_porosity] +\
-        [fruit_color, fruit_conspicuous] + \
-        [growth_form, growth_rate, height] + \
-        [leaf_retention, lifespan, toxicity, shape_and_orientation] + \
-        [low_growing_grass, drought, fertility] + \
-        [growth_form, growth_rate, fire_tolerance, precipitation, shade, temperature, bulb, palatable]
+    L = [s_category] 
+        #+ [duration, habit, growth_period, fall_conspicuous] + [flower_color, flower_conspicuous] + \
+        #[foliage_color, foliage_texture, foliage_porosity] +\
+        #[fruit_color, fruit_conspicuous] + \
+        #[growth_form, growth_rate, height] + \
+        #[leaf_retention, lifespan, toxicity, shape_and_orientation] + \
+        #[low_growing_grass, drought, fertility] + \
+        #[growth_form, growth_rate, fire_tolerance, precipitation, shade, temperature, bulb, palatable]
 
     # for now, we choose to ignore the sparse entries
     L = [l for l in L if 'unknown' not in l and ' nan ' not in l and l != '']
